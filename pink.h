338ad55fe1f5aaa55b260d7d36381daf332d3bc2
Patrick Ingham <emmanuel.goldstien@gmail.com>
2014-12-20 13:10:12 -0700

    Brought client side into more line with server

---
+++ pink.h
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h> 
+
+#ifndef HEY_H
+#define HEY_H
+
+void error(const char);
+
+#endif
